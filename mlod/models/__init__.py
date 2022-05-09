2225ecf2116a221aeae010c7435713ca36f2b3f7
Martinszy007 <martinszy@outlook.com>
2022-05-09 14:31:55 +0200

    second

---
+++ mlod/models/__init__.py
@@ -0,0 +1,7 @@
+# base model
+from .model import Model
+
+# Setting the ML models 
+# we implemented in the challenge
+from .booster_models import LGBModel
+from .booster_models import CatBoostModel
